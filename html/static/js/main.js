2d5a5eb3a9f20155d4e74a821c5d8f83df401749
KingOfRostov <31827188+KingOfRostov@users.noreply.github.com>
2019-03-12 21:08:09 +0300

    Requirements and errors
    requirements.txt was added.
    try exception block has been fixed

--- html/static/js/main.js
+++ html/static/js/main.js
@@ -2,7 +2,7 @@
 var socket = new WebSocket("ws://localhost:8000/dockcontrol");
 //Ask server to send the information about existing containers and images
 socket.onopen = function() {
-  socket.send("Info")
+  socket.send("Info");
 };
 
 // socket.onclose = function(event) {
@@ -32,99 +32,105 @@ socket.onmessage = function(event) {
   $(ul_containers).children().remove();
   ul_images.appendChild(image_title);
   ul_containers.appendChild(container_title);
-
-  //Parse data to isolate the information about images from information about containers
-  for (p in par){
-    //Create the image element with button "Run"
-    if (par[p]["name"] == "image"){
-      var li = document.createElement('li');
-      li.setAttribute("class", "list-group-item");
-      li.id = par[p]["inner"]["image_name"];
-      li.innerText = par[p]["inner"]["image_name"];
-
-      var p = document.createElement('p');
-      p.setAttribute("class", "mt-2");
-
-      var buttonRun = document.createElement('button');
-      buttonRun.setAttribute("class", "btn btn-success");
-      buttonRun.setAttribute("type", "button");
-      buttonRun.setAttribute("value", "Run");
-      buttonRun.setAttribute("name", "Run");
-      buttonRun.setAttribute("onclick", "container_docker('" + li.id + "')");
-      buttonRun.innerText = "Run";
-
-      p.appendChild(buttonRun);
-      li.appendChild(p);
-      ul_images.appendChild(li);
-    }
-    else {
-      //Create the container element with buttons "Stop" and "Delete"
-      //if the container's working
-      if (par[p]["inner"]["status"] == "up" || par[p]["inner"]["status"] == "running"){
-        var li = document.createElement('li');
-        li.setAttribute("class", "list-group-item");
-        li.id = par[p]["inner"]["container_name"];
-        li.innerText = par[p]["inner"]["container_name"];
-
-        var p = document.createElement('p');
-        p.setAttribute("class", "mt-2");
-
-        var buttonStop = document.createElement('button');
-        buttonStop.setAttribute("class", "btn btn-warning");
-        buttonStop.setAttribute("type", "button");
-        buttonStop.setAttribute("value", "Stop");
-        buttonStop.setAttribute("name", "Stop");
-        buttonStop.setAttribute("onclick", "stop_docker('" + li.id + "')");
-        buttonStop.innerText = "Stop";
-
-        var buttonDelete = document.createElement('button');
-        buttonDelete.setAttribute("class", "btn btn-danger ml-1");
-        buttonDelete.setAttribute("type", "button");
-        buttonDelete.setAttribute("value", "Delete");
-        buttonDelete.setAttribute("name", "Delete");
-        buttonDelete.setAttribute("onclick", "delete_docker('" + li.id + "')");
-        buttonDelete.innerText = "Delete";
-
-        p.appendChild(buttonStop);
-        p.appendChild(buttonDelete);
-        li.appendChild(p);
-        ul_containers.appendChild(li);
-      }
-      //Create the container element with buttons "Start" and "Delete"
-      //if the container's stopped
-      else {
-        var li = document.createElement('li');
-        li.setAttribute("class", "list-group-item");
-        li.id = par[p]["inner"]["container_name"];
-        li.innerText = par[p]["inner"]["container_name"];
-
-        var p = document.createElement('p');
-        p.setAttribute("class", "mt-2");
-
-        var buttonStart = document.createElement('button');
-        buttonStart.setAttribute("class", "btn btn-success");
-        buttonStart.setAttribute("type", "button");
-        buttonStart.setAttribute("value", "Start");
-        buttonStart.setAttribute("name", "Start");
-        buttonStart.setAttribute("onclick", "start_docker('" + li.id + "')");
-        buttonStart.innerText = "Start";
-
-        var buttonDelete = document.createElement('button');
-        buttonDelete.setAttribute("class", "btn btn-danger ml-1");
-        buttonDelete.setAttribute("type", "button");
-        buttonDelete.setAttribute("value", "Delete");
-        buttonDelete.setAttribute("name", "Delete");
-        buttonDelete.setAttribute("onclick", "delete_docker('" + li.id + "')");
-        buttonDelete.innerText = "Delete";
-
-        p.appendChild(buttonStart);
-        p.appendChild(buttonDelete);
-        li.appendChild(p);
-        ul_containers.appendChild(li);
-      }
-    };
+  if (par.includes("Invalid request")){
+    alert(par);
   }
+  else{
+    //Parse data to isolate the information about images from information about containers
+    for (p in par){
+      //Create the image element with button "Run"
+      if (par[p]["name"] == "image"){
+        var li = document.createElement('li');
+        li.setAttribute("class", "list-group-item");
+        li.id = par[p]["inner"]["image_name"];
+        li.innerText = par[p]["inner"]["image_name"];
 
+        var p = document.createElement('p');
+        p.setAttribute("class", "mt-2");
+
+        var buttonRun = document.createElement('button');
+        buttonRun.setAttribute("class", "btn btn-success");
+        buttonRun.setAttribute("type", "button");
+        buttonRun.setAttribute("value", "Run");
+        buttonRun.setAttribute("name", "Run");
+        buttonRun.setAttribute("onclick", "container_docker('" + li.id + "')");
+        buttonRun.innerText = "Run";
+
+        p.appendChild(buttonRun);
+        li.appendChild(p);
+        ul_images.appendChild(li);
+      }
+      // else if (par[p]["name"] == "listening_port") {
+      //   alert("port " + par[p]["port"]);
+      // }
+      else if (par[p]["name"] == "container"){
+        //Create the container element with buttons "Stop" and "Delete"
+        //if the container's working
+        if (par[p]["inner"]["status"] == "up" || par[p]["inner"]["status"] == "running"){
+          var li = document.createElement('li');
+          li.setAttribute("class", "list-group-item");
+          li.id = par[p]["inner"]["container_name"];
+          li.innerText = par[p]["inner"]["container_name"];
+
+          var p = document.createElement('p');
+          p.setAttribute("class", "mt-2");
+
+          var buttonStop = document.createElement('button');
+          buttonStop.setAttribute("class", "btn btn-warning");
+          buttonStop.setAttribute("type", "button");
+          buttonStop.setAttribute("value", "Stop");
+          buttonStop.setAttribute("name", "Stop");
+          buttonStop.setAttribute("onclick", "stop_docker('" + li.id + "')");
+          buttonStop.innerText = "Stop";
+
+          var buttonDelete = document.createElement('button');
+          buttonDelete.setAttribute("class", "btn btn-danger ml-1");
+          buttonDelete.setAttribute("type", "button");
+          buttonDelete.setAttribute("value", "Delete");
+          buttonDelete.setAttribute("name", "Delete");
+          buttonDelete.setAttribute("onclick", "delete_docker('" + li.id + "')");
+          buttonDelete.innerText = "Delete";
+
+          p.appendChild(buttonStop);
+          p.appendChild(buttonDelete);
+          li.appendChild(p);
+          ul_containers.appendChild(li);
+        }
+        //Create the container element with buttons "Start" and "Delete"
+        //if the container's stopped
+        else {
+          var li = document.createElement('li');
+          li.setAttribute("class", "list-group-item");
+          li.id = par[p]["inner"]["container_name"];
+          li.innerText = par[p]["inner"]["container_name"];
+
+          var p = document.createElement('p');
+          p.setAttribute("class", "mt-2");
+
+          var buttonStart = document.createElement('button');
+          buttonStart.setAttribute("class", "btn btn-success");
+          buttonStart.setAttribute("type", "button");
+          buttonStart.setAttribute("value", "Start");
+          buttonStart.setAttribute("name", "Start");
+          buttonStart.setAttribute("onclick", "start_docker('" + li.id + "')");
+          buttonStart.innerText = "Start";
+
+          var buttonDelete = document.createElement('button');
+          buttonDelete.setAttribute("class", "btn btn-danger ml-1");
+          buttonDelete.setAttribute("type", "button");
+          buttonDelete.setAttribute("value", "Delete");
+          buttonDelete.setAttribute("name", "Delete");
+          buttonDelete.setAttribute("onclick", "delete_docker('" + li.id + "')");
+          buttonDelete.innerText = "Delete";
+
+          p.appendChild(buttonStart);
+          p.appendChild(buttonDelete);
+          li.appendChild(p);
+          ul_containers.appendChild(li);
+        }
+      };
+    }
+  }
   //Show client corresponding information if there are no images on server
   if ($(ul_images).children().length == 1){
     var li_image = document.createElement("li");
--- server.py
+++ server.py
@@ -1,91 +1,128 @@
+# -*- coding: utf-8 -*-
 from sanic import Sanic
-import sanic
 import json
+import sanic
 import docker
+import sys
 
 client = docker.from_env(version='auto')
 
-#Removes existing container by its name
+
+# Removes existing container by its name
+
 def delete_container(name):
-    container = client.containers.get(name)
-    container.stop()
-    container.remove()
-    return get_info()
+    for i in client.containers.list(all=True):
+        if name in i.name:
+            container = client.containers.get(name)
+            container.stop()
+            container.remove()
+            return get_info()
+    else:
+        return("Invalid request. There is no container with name : " + name)
 
-#Stops running container by its name
+
+# Stops running container by its name
+
 def stop_container(name):
-    container = client.containers.get(name)
-    container.stop()
-    return get_info()
+    for i in client.containers.list(all=True):
+        if name in i.name:
+            container = client.containers.get(name)
+            container.stop()
+            return get_info()
+    else:
+        return("Invalid request. There is no container with name : " + name)
 
-#Starts existing container, for instance : with status "Exited"
+
+# Starts existing container, for instance : with status "Exited"
+
 def start_container(name):
-    client.api.start(name)
-    return get_info()
+    for i in client.containers.list(all=True):
+        if name in i.name:
+            client.api.start(name)
+            return get_info()
+    else:
+        return("Invalid request. There is no container with name : " + name)
 
-#Runs a container inside chosen image
+
+# Runs a container inside chosen image
+
 def run_container(image):
-    client.containers.run(image, detach=True)
-    return get_info()
+    for i in set(client.images.list()):
+        if image in str(i):
+            client.containers.run(image, detach=True)
+            return get_info()
+    else:
+        return("Invalid request. There is no image with name : " + image)
 
-#Returns the names of existing containers and their statuses
+
+# Returns the names of existing containers and their statuses
+
 def get_containers_info():
     res = []
     for container in client.containers.list(all=True):
-        var_dict_cont = {
-            "container_name" : str(container.name) + ' : ' + str(container.image).split(":")[1][2:] ,
-            "status" : container.status ,
-        }
-        var_dict = {
-            "name" : "container",
-            "inner" : var_dict_cont,
-        }
+        var_dict_cont = {'container_name': str(container.name) + ' : '
+                         + (str(container.image).split(':')[1])[2:],
+                         'status': container.status}
+        var_dict = {'name': 'container', 'inner': var_dict_cont}
         res.append(var_dict)
     return res
 
-#Returns the names of existing images
+
+# Returns the names of existing images
+
 def get_images_info():
     res = []
     for image in set(client.images.list()):
-        var_dict_image = {
-            "image_name" : str(image).split(':')[1][2:] ,
-        }
-        var_dict = {
-            "name" : "image",
-            "inner" : var_dict_image
-        }
+        var_dict_image = {'image_name': (str(image).split(':')[1])[2:]}
+        var_dict = {'name': 'image', 'inner': var_dict_image}
         res.append(var_dict)
     return res
 
-#Returns the names of existing images and the names of existing containers
-#with their statuses
+
+# Returns the names of existing images and the names of existing containers
+# with their statuses
+
 def get_info():
     conts = get_containers_info()
     ims = get_images_info()
-    return  conts + ims
+    lis_port = [{'name': 'listening_port', 'port': port}]
+    return conts + ims + lis_port
 
 
 app = Sanic()
 
-#Start websocket at localhost:8000/dockcontrol
+
+# Start websocket at localhost:8000/dockcontrol
+
 @app.websocket("/dockcontrol")
 async def docker_control(request, ws):
-    #Send the information about existing containers and images to client with JSON
+
+    # Send the information about existing containers and images to client
+    # with JSON
+
     await ws.send(json.dumps(get_containers_info()))
-    #Listening for client's messages in while loop
+
+    # Listening for client's messages in while loop
+
     while True:
-        #Receive the message
+        # Receive the message
         data = await ws.recv()
+        print(f"Got the input from the client: {data}")
         '''
         We got a list of possible commands:
-        1) "Info" - returns the information about existing images and containers
+        1) "Info" - returns the information about existing
+            images and containers
         2) "Run image_name" - runs a container within the image with
             the name "image_name"
-        3) "Delete container_name" - removes a container with the name "container_name"
-        4) "Stop container_name" - stops a container with the name "container_name"
-        5) "Start container_name" - starts a container with the name "container_name"
-
+        3) "Delete container_name" - removes a container
+            with the name "container_name"
+        4) "Stop container_name" - stops a container
+            with the name "container_name"
+        5) "Start container_name" - starts a container
+            with the name "container_name"
+        6) "Port" - returns listening port
         '''
+
         if len(data.split()) == 1 and data.split()[0] == "Info":
             await ws.send(json.dumps(get_info()))
         else:
@@ -101,10 +138,15 @@ async def docker_control(request, ws):
                 elif command == "Start":
                     await ws.send(json.dumps(start_container(name)))
                 else:
-                    await ws.send(json("Invalid request : " + command))
-            except:
-                pass
-        print(f"Got the input from the client: {data}")
+                    await ws.send(json.dumps("Invalid request : " + data))
+            except IndexError:
+                print('Index error with the input : ' + data)
+                await ws.send(json.dumps("Invalid request : " + data))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    if len(sys.argv) == 2:
+        port = sys.argv[1]
+    else:
+        port = 8000
+    app.run(host='0.0.0.0', port=port)
